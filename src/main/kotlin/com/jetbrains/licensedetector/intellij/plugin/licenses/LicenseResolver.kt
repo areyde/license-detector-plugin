7e2f245d57dea80ceca230cedd8aca71d322d6c6
Dmitry Pogrebnoy <pogrebnoy.inc@gmail.com>
2020-11-13 12:03:43 +0300

    Add license matching on name

--- src/main/kotlin/com/jetbrains/licensedetector/intellij/plugin/licenses/LicenseResolver.kt
+++ src/main/kotlin/com/jetbrains/licensedetector/intellij/plugin/licenses/LicenseResolver.kt
@@ -1,6 +1,6 @@
 package com.jetbrains.licensedetector.intellij.plugin.licenses
 
-fun getLicenseOnSpdxIdOrNull(spdxId: String): License? {
+internal fun getLicenseOnSpdxIdOrNull(spdxId: String): SupportedLicense? {
     return when (spdxId) {
         Apache_2_0.spdxId -> Apache_2_0
         BSD_3_Clause.spdxId -> BSD_3_Clause
@@ -11,7 +11,18 @@ fun getLicenseOnSpdxIdOrNull(spdxId: String): License? {
     }
 }
 
-fun getCompatiblePackageLicenses(projectLicenses: Set<SupportedLicense>): List<SupportedLicense> {
+internal fun getLicenseOnNameOrNull(licenseName: String): SupportedLicense? {
+    return when (licenseName) {
+        Apache_2_0.name -> Apache_2_0
+        BSD_3_Clause.name -> BSD_3_Clause
+        GPL_3_0_or_later.name -> GPL_3_0_or_later
+        LGPL_2_1_or_later.name -> LGPL_2_1_or_later
+        MIT.name -> MIT
+        else -> null
+    }
+}
+
+internal fun getCompatiblePackageLicenses(projectLicenses: Set<SupportedLicense>): List<SupportedLicense> {
     return projectLicenses.map { it.compatiblePackageLicenses }.reduce { acc, set ->
         acc.intersect(set)
     }.toList().sortedBy { it.name }
--- src/main/kotlin/com/jetbrains/packagesearch/intellij/plugin/api/PackageLicenseAdapter.kt
+++ src/main/kotlin/com/jetbrains/packagesearch/intellij/plugin/api/PackageLicenseAdapter.kt
@@ -5,6 +5,7 @@ import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import com.jetbrains.licensedetector.intellij.plugin.licenses.License
 import com.jetbrains.licensedetector.intellij.plugin.licenses.UnsupportedLicense
+import com.jetbrains.licensedetector.intellij.plugin.licenses.getLicenseOnNameOrNull
 import com.jetbrains.licensedetector.intellij.plugin.licenses.getLicenseOnSpdxIdOrNull
 import java.lang.reflect.Type
 
@@ -15,11 +16,21 @@ object PackageLicenseAdapter : JsonDeserializer<License> {
             type: Type,
             jsonDeserializationContext: JsonDeserializationContext): License {
         val jsonObject = jsonElement.asJsonObject
-        return if (jsonObject.has("spdx_id")) {
-            getLicenseOnSpdxIdOrNull(jsonObject["spdx_id"].asString)
-                    ?: jsonDeserializationContext.deserialize(jsonElement, UnsupportedLicense::class.java)
-        } else {
-            jsonDeserializationContext.deserialize(jsonElement, UnsupportedLicense::class.java)
+
+        if (jsonObject.has("spdx_id")) {
+            val license = getLicenseOnSpdxIdOrNull(jsonObject["spdx_id"].asString)
+            if (license != null) {
+                return license
+            }
         }
+
+        if (jsonObject.has("name")) {
+            val license = getLicenseOnNameOrNull(jsonObject["name"].asString)
+            if (license != null) {
+                return license
+            }
+        }
+
+        return jsonDeserializationContext.deserialize(jsonElement, UnsupportedLicense::class.java)
     }
 }
